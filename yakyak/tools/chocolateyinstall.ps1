c01bb1b9d2f8aa39ca0d7f9e68e4b28f0fd41279
Ismael Reyes <ismael.kix2902@gmail.com>
2020-07-11 14:36:53 +0200

    First commit for YakTak

---
+++ yakyak/tools/chocolateyinstall.ps1
@@ -0,0 +1,22 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName  = 'yakyak' 
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url          = 'https://github.com/yakyak/yakyak/releases/download/v1.5.4-beta-rolling/yakyak-1.5.4-win32-x64.zip'
+$checksum     = '756EE0758C7B30FFF11F442A611D38CF21DCF63BBD22AC4D2EDE64FB15219077'
+$ShortcutName = 'YakYak.lnk'
+$unzipDir     = 'YakYak-win32-x64'
+$exe          = 'YakYak.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'ZIP' 
+  url           = $url
+  checksum      = $checksum
+  checksumType  = 'sha256'    
+}
+
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:Public\Desktop\$ShortcutName" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
+Install-ChocolateyShortcut -shortcutFilePath "$ENV:ProgramData\Microsoft\Windows\Start Menu\Programs\$ShortcutName" -targetPath "$toolsDir\$unzipDir\$exe" -WorkingDirectory "$toolsDir\$unzipDir"
+
